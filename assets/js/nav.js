91a9ba93fd9f2bab90d761adfee93b7a96ae2958
Ji99 <rksk0531@naver.com>
2022-02-23 18:27:04 +0900

    commit

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -1,3 +1,24 @@
 const navBar = document.querySelector(".nav__bar");
 
 const navContainer = document.querySelector(".nav__container");
+
+let startPos = 0;
+let endPos = 0;
+let curPos = 0;
+
+navBar.addEventListener("touchstart", handleStart);
+navBar.addEventListener("touchmove", handleMove);
+navBar.addEventListener("touchend", handleEnd);
+
+function handleStart(evt){
+    startPos = evt.changedTouches[0];
+    console.log("startPos: ",startPos);
+}
+
+function handleMove(evt){
+    curPos
+}
+
+function handleEnd(evt){
+
+}
