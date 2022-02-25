30dc418f0a19c839f125d2f8860af1a55b1e2fdf
Ji99 <rksk0531@naver.com>
2022-02-25 22:13:22 +0900

    commit

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -7,45 +7,50 @@ let endPos = 0;
 let curPos = 0;
 let offset = 0;
 
-const deg = parseInt(getComputedStyle(sectionNav).getPropertyValue("transform").match(/(-?[0-9\.]+)/g)[4]);
+const deg = parseInt(
+  getComputedStyle(sectionNav)
+    .getPropertyValue("transform")
+    .match(/(-?[0-9\.]+)/g)[4]
+);
+// 숨기기 위해 이동한 값
 console.log(deg);
 
 sectionNav.addEventListener("touchstart", handleStart);
 sectionNav.addEventListener("touchmove", handleMove);
 sectionNav.addEventListener("touchend", handleEnd);
 
+//Touch Start
 function handleStart(evt) {
-  startPos = evt.changedTouches[0].pageX;
+  startPos = evt.changedTouches[0].pageX; //터치 시작 좌표
   curPos = startPos;
   console.log("startPos: ", startPos);
   if (navBar.classList.contains("hidden")) {
     navBar.classList.remove("hidden");
   }
 }
-
+// Touch ing....~
 function handleMove(evt) {
   console.log("curPos: ", curPos);
-  curPos = evt.changedTouches[0].pageX;
+  curPos = evt.changedTouches[0].pageX; // 터치중 이동중 좌표
   offset = deg + curPos;
   console.log("offset: ", offset);
   if (offset >= 0) {
     sectionNav.style.transform = "translateX(0)";
+  } else if (offset <= deg) {
+    sectionNav.style.transform = `translateX(-${deg}px)`;
   } else {
     sectionNav.style.transform = `translateX(${offset}px)`;
   }
 }
 
+//Touch End
 function handleEnd(evt) {
-  endPos = evt.changedTouches[0].pageX;
-  console.log("endPos: ",endPos);
-  if (endPos > Math.abs(deg)/2) {
+  endPos = evt.changedTouches[0].pageX; //터지 종료 좌표
+  console.log("endPos: ", endPos);
+  if (endPos > Math.abs(deg) / 2) {
     sectionNav.style.transform = "translateX(0)";
     navBar.classList.add("hidden");
   } else {
-    sectionNav.style.transform = `translateX(-${deg}px)`;
-  }
-
-  if (endPos >= Math.abs(deg) - 20) {
-    navBar.classList.add("hidden");
+    sectionNav.style.transform = `translateX(${deg}px)`;
   }
 }
